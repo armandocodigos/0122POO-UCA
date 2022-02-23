94c92e9d88fcaeee04f676e1d284e2939bfe23da
Armando Codigos <rcanizales@uca.edu.sv>
2022-02-23 16:25:32 -0600

    Agregar T1 y Hola Mundo

---
+++ T1/HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
