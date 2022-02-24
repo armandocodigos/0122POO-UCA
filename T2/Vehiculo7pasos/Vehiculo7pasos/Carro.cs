f1834ede4f23332ea2e771d1c7021ab3736f9e4c
Armando Codigos <rcanizales@uca.edu.sv>
2022-02-24 11:21:25 -0600

    Añadir ejemplo 7 pasos

---
+++ T2/Vehiculo7pasos/Vehiculo7pasos/Carro.cs
@@ -0,0 +1,36 @@
+namespace Vehiculo7pasos
+{
+    //Un carro que avanza y consume gasolina
+    public class Carro
+    {
+        private double gasolina;
+        private double eficiencia;
+
+        public Carro(double pEficiencia)
+        {
+            gasolina = 0;
+            eficiencia = pEficiencia;
+        }
+
+        public Carro(double gasolina, double eficiencia)
+        {
+            this.gasolina = gasolina;
+            this.eficiencia = eficiencia;
+        }
+
+        public void ponerGasolina(double cantidad)
+        {
+            gasolina += cantidad;
+        }
+
+        public void avanzar(double distancia)
+        {
+            gasolina -= (distancia / eficiencia);
+        }
+
+        public double consultarGasolina()
+        {
+            return gasolina;
+        }
+    }
+}
---
+++ T2/Vehiculo7pasos/Vehiculo7pasos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Vehiculo7pasos
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Carro unCarro = new Carro(10);
+
+            unCarro.ponerGasolina(20);
+            unCarro.avanzar(100);
+            Console.Write("Gasolina restante: ");
+            Console.Write(unCarro.consultarGasolina());
+            Console.WriteLine(" galones.");
+        }
+    }
+}
