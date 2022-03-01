59ce915f18a28846958a5f5a275c59a09f9e1422
Armando Codigos <rcanizales@uca.edu.sv>
2022-03-01 08:48:29 -0600

    Añadir ejemplo input usuario

---
+++ T1/InputUsuario/InputUsuario/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InputUsuario
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            String inputCadena = "";
+            Console.Write("Digite una cadena: ");
+            inputCadena = Console.ReadLine();
+
+            int inputEntero = 0;
+            Console.Write("Digite un entero: ");
+            inputEntero = Convert.ToInt32(Console.ReadLine());
+
+            double inputDouble = 0.0;
+            Console.Write("Digite un real: ");
+            inputDouble = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Cadena: " + inputCadena);
+            Console.WriteLine("Doble entero: " + (2 * inputEntero));
+            Console.WriteLine("Triple punto flotante: " + (3 * inputDouble));
+        }
+    }
+}
