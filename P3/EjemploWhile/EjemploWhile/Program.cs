844840530387991ecaab8358d4d72a9245a2687e
Armando Codigos <rcanizales@uca.edu.sv>
2022-04-05 11:30:29 -0600

    Añadir Ejemplo while

---
+++ P3/EjemploWhile/EjemploWhile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EjemploWhile
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Instrucciones antes del try.");
+        
+            bool finalizado = false;
+            while(!finalizado){
+                try{
+                    // Podría generar excepciones
+                    Console.Write("\n\nDigite el numerador: ");
+                    int numerador = Convert.ToInt32(Console.ReadLine());
+    	        
+                    Console.Write("Digite el denominador: ");
+                    int denominador = Convert.ToInt32(Console.ReadLine());
+    	        
+                    double resultado = numerador / denominador;
+    	        
+                    Console.WriteLine("Resultado: " + resultado);
+                    finalizado = true;
+                }
+                catch(DivideByZeroException ex){
+                    Console.WriteLine("Error, división entre cero!");
+                    Console.WriteLine("Favor intente con otros valores.");
+                }
+                catch(FormatException ex){
+                    Console.WriteLine("Error, tipo de dato no válido.");
+                }
+            }
+        
+            Console.WriteLine("Instrucciones después del try.");
+        }
+    }
+}
