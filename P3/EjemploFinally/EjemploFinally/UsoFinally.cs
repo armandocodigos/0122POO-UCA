f6aca6bb76965ad6e96f56718a8e6497c93a7474
Armando Codigos <rcanizales@uca.edu.sv>
2022-04-05 12:01:55 -0600

    Modificar Ejemplo

--- P3/EjemploFinally/EjemploFinally/UsoFinally.cs
+++ P3/EjemploFinally/EjemploFinally/UsoFinally.cs
@@ -17,13 +17,13 @@ namespace EjemploFinally
                         nums[4] = 4; //Genera un error de indexación
                         break;
                     case 2:
-                        return; //Retorna desde el bloque try
+                        Console.WriteLine("Hola, no hay error!");
+                        break; //Retorna desde el bloque try
                 }
             }
             catch (DivideByZeroException exc){
                 //Capturando la excepción
                 Console.WriteLine("No se puede dividir por cero!");
-                return; //retorna desde catch
             }
             catch (IndexOutOfRangeException exc){
                 //Capturando la excepción
@@ -31,7 +31,7 @@ namespace EjemploFinally
             }
             finally {
                 //Esto se ejecuta al salir de los bloques try-catch
-                Console.WriteLine("Saliendo de try.");
+                Console.WriteLine("Ejecutando el finally.");
             }
         }
     }
