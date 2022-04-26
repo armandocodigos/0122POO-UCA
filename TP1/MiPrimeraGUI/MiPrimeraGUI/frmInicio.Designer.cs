51ac010756e980eca59b77dc0e7e360b48fee1ff
Armando Codigos <rcanizales@uca.edu.sv>
2022-04-26 15:26:07 -0600

    Añadir Ejemplo 26Abril

---
+++ TP1/MiPrimeraGUI/MiPrimeraGUI/frmInicio.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MiPrimeraGUI
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicio));
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.nudEdad = new System.Windows.Forms.NumericUpDown();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEdad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.Color.Sienna;
+            this.lblNombre.Location = new System.Drawing.Point(26, 45);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(99, 26);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            this.lblNombre.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(131, 43);
+            this.txtNombre.MaxLength = 30;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(144, 29);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnSaludar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaludar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaludar.Location = new System.Drawing.Point(297, 38);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(166, 41);
+            this.btnSaludar.TabIndex = 2;
+            this.btnSaludar.Text = "Saludar";
+            this.btnSaludar.UseVisualStyleBackColor = false;
+            this.btnSaludar.Click += new System.EventHandler(this.btnSaludar_Click);
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaludo.ForeColor = System.Drawing.Color.Sienna;
+            this.lblSaludo.Location = new System.Drawing.Point(469, 45);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(198, 26);
+            this.lblSaludo.TabIndex = 3;
+            this.lblSaludo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.ForeColor = System.Drawing.Color.Sienna;
+            this.lblEdad.Location = new System.Drawing.Point(26, 176);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(99, 26);
+            this.lblEdad.TabIndex = 4;
+            this.lblEdad.Text = "Edad:";
+            this.lblEdad.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // nudEdad
+            // 
+            this.nudEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudEdad.Location = new System.Drawing.Point(131, 175);
+            this.nudEdad.Maximum = new decimal(new int[] { 30, 0, 0, 0 });
+            this.nudEdad.Minimum = new decimal(new int[] { 16, 0, 0, 0 });
+            this.nudEdad.Name = "nudEdad";
+            this.nudEdad.Size = new System.Drawing.Size(144, 29);
+            this.nudEdad.TabIndex = 5;
+            this.nudEdad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudEdad.Value = new decimal(new int[] { 16, 0, 0, 0 });
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(297, 169);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(275, 41);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "¿Mayor de edad?";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(694, 271);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.nudEdad);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblSaludo);
+            this.Controls.Add(this.btnSaludar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bienvenidos a mi primer GUI en C#!";
+            ((System.ComponentModel.ISupportInitialize)(this.nudEdad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btnCalcular;
+
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.NumericUpDown nudEdad;
+
+        private System.Windows.Forms.Label lblSaludo;
+
+        private System.Windows.Forms.Button btnSaludar;
+
+        private System.Windows.Forms.TextBox txtNombre;
+
+        private System.Windows.Forms.Label lblNombre;
+
+        #endregion
+    }
+}
---
+++ TP1/MiPrimeraGUI/MiPrimeraGUI/frmInicio.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimeraGUI
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+            //Lo que se coloque aquí, se ejecutará cuando se presione el botón
+            //Es decir, cuando suceda el evento "Click" para el control "btnSaludar"
+
+            lblSaludo.Text = "Hola " + txtNombre.Text + "!";
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            //Este evento se ejecuta cada vez que el usuario modifica el Text del TextBox
+            lblSaludo.Text = "Escribiendo...";
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int edad = Convert.ToInt32(nudEdad.Value);
+
+            if (edad >= 18)
+                //Mostrar mensaje de mayor de edad
+                MessageBox.Show("Usted es mayor de edad!", "POO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                //Mostrar mensaje de menor de edad
+                MessageBox.Show("Usted es menor de edad?", "POO", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+    }
+}
