aa6b6593e7c022a95c44d634ec893faa37e14161
Armando Codigos <rcanizales@uca.edu.sv>
2022-05-31 15:52:52 -0600

    Añadir consultas SQL

---
+++ TP7/SQLQuery_1.sql
@@ -0,0 +1,7 @@
+SELECT CategoryID, CategoryName, Description FROM Categories;
+
+SELECT CategoryID, CategoryName, Description FROM Categories
+order by CategoryName asc;
+
+SELECT CategoryID, CategoryName, Description FROM Categories
+where CategoryID = 3;
